3bc07708b27a849b9affd41710d2d895067a6d13
Colin Travis <colint@cybertravis.us>
2017-04-03 15:26:52 -0500

    Should have a working one now.. building to Heroku

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,22 +1,12 @@
-
-console.log('quicktest');
-console.log('serial list:');
-p5.serial().list(function(data) {
-  data.ports.forEach(function(port) {
-    console.log(port.comName);
-  });
-});
-
 // var b = p5.board('/dev/cu.usbmodem1421', 'arduino');
 
-var led;
+// var led;
 var lightStatus = false;
-var cfade3 = 138;
-var cfade2 = 20;
-var cfade1 = 58;
-var fade = 'fade';
+// var cfade3 = 138;
+// var cfade2 = 20;
+// var cfade1 = 58;
+// var fade = 'fade';
 var tone;
-
 
 
 
@@ -25,44 +15,25 @@ function preload(){
 }
 
 function setup() {
- // led = b.pin(9, 'LED');
+//  // led = b.pin(9, 'LED');
  createCanvas(windowWidth,windowHeight)
  background(70,70,70)
- tone.loop()
+ // tone.loop()
 }
-
-// function draw(){
-//   console.log(lightStatus);
-//   if (lightStatus === true) {
-//     background(58, 20, 138)
-//   }else if (lightStatus === false){
-//     background(0,0,0)
-//   }else if (lightStatus === fade) {
-//     background(cfade1,cfade2,cfade3)
-//     if (cfade3 <= 0) {
-//       cfade3 = 138;
-//       cfade2 = 20;
-//       cfade1 = 58;
-//     }else {
-//     cfade1--
-//     cfade2--
-//     cfade3--
-//   }
-// }
-//
-// }
 
 function draw(){
   if (keyIsDown(65)){
     // led.off();
     tone.stop()
     background(255,0,0)
+    console.log("Key is down!");
     // lightStatus = true;
   } else {
     // led.on();
     // led.write(200)
     tone.play()
     background(0,0,0)
+    console.log("Key is up!");
     // lightStatus = false;
   }
 }
