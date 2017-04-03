69f4a4f23339e83e313a5f965821fa4eac4d11d0
Colin Travis <colint@cybertravis.us>
2017-04-03 14:37:09 -0500

    Close now...

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,4 +1,16 @@
 // var b = p5.board('/dev/cu.usbmodem1421', 'arduino');
+var express = require('express')
+var app = express();
+//
+var port = process.env.PORT || 4000
+var server = require('http').Server( app )
+server.listen(port, function(){
+  console.log("App is running on port" + port);
+})
+
+app.use ( express.static('sketch') )
+
+
 var led;
 var lightStatus = false;
 var cfade3 = 138;
@@ -7,8 +19,11 @@ var cfade1 = 58;
 var fade = 'fade';
 var tone;
 
+
+
+
 function preload(){
-  tone = loadSound("/assets/sounds/ominous_tone.mp3")
+  tone = loadSound("assets/sounds/ominous_tone.mp3")
 }
 
 function setup() {
@@ -43,11 +58,13 @@ function draw(){
   if (keyIsDown(65)){
     // led.off();
     tone.stop()
+    background(255,0,0)
     // lightStatus = true;
   } else {
     // led.on();
     // led.write(200)
     tone.play()
+    background(0,0,0)
     // lightStatus = false;
   }
 }
