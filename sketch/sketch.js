d580a463b432c7fe54a2d0f117c22e886298b19f
Colin Travis <colint@cybertravis.us>
2017-04-03 22:53:50 -0500

    turned back on LED

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,7 +1,7 @@
-// var b = p5.board('/dev/cu.usbmodem1421', 'arduino');
+var b = p5.board('/dev/cu.usbmodem1421', 'arduino');
 
-// var led;
-var lightStatus = false;
+var led;
+// var lightStatus = false;
 // var cfade3 = 138;
 // var cfade2 = 20;
 // var cfade1 = 58;
@@ -15,7 +15,7 @@ function preload(){
 }
 
 function setup() {
-//  // led = b.pin(9, 'LED');
+ // led = b.pin(9, 'LED');
  createCanvas(windowWidth,windowHeight)
  background(70,70,70)
  // tone.loop()
@@ -23,21 +23,20 @@ function setup() {
 
 function draw(){
   if (keyIsDown(65)){
-    // led.off();
+    led.off();
     tone.stop()
     background(255,0,0)
     console.log("Key is down!");
     // lightStatus = true;
   } else {
-    // led.on();
-    // led.write(200)
+    led.on();
+    led.write(200)
     tone.play()
     background(0,0,0)
     console.log("Key is up!");
     // lightStatus = false;
   }
 }
-
 
 function windowResized(){
 	resizeCanvas(windowWidth,windowHeight)
